d8000de3f2c0e583dbd9e1a92a0d7c92ede398f0
jakeocinco <jakeocinco@me.com>
2026-05-09 08:17:27 -0700

    Add four Lambda functions with cross-invoke permissions (0.3)

--- infra/lib/stack.ts
+++ infra/lib/stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from "aws-cdk-lib";
 import * as s3 from "aws-cdk-lib/aws-s3";
+import * as lambda from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs";
 
 export interface DakoderStackProps extends cdk.StackProps {
@@ -16,5 +17,38 @@ export class DakoderStack extends cdk.Stack {
       bucketName: `${props.stackTag}-workspace`,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
+
+    const functionNames = ["orchestrator", "code", "build", "review"] as const;
+
+    const functions = Object.fromEntries(
+      functionNames.map((name) => {
+        const fn = new lambda.Function(this, `${name}-fn`, {
+          functionName: `${props.stackTag}-dakoder-${name}`,
+          runtime: lambda.Runtime.NODEJS_22_X,
+          handler: "index.handler",
+          code: lambda.Code.fromInline(
+            'exports.handler = async () => ({ statusCode: 200 });'
+          ),
+          timeout: cdk.Duration.minutes(15),
+          environment: {
+            BUCKET_NAME: this.bucket.bucketName,
+            STACK_TAG: props.stackTag,
+          },
+        });
+        this.bucket.grantReadWrite(fn);
+        return [name, fn];
+      })
+    ) as Record<(typeof functionNames)[number], lambda.Function>;
+
+    for (const name of functionNames) {
+      for (const other of functionNames) {
+        if (other === name) continue;
+        functions[name].addEnvironment(
+          `${other.toUpperCase()}_FUNCTION_NAME`,
+          functions[other].functionName
+        );
+        functions[other].grantInvoke(functions[name]);
+      }
+    }
   }
 }
